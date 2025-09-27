a4e3cf9e87e0880189293616d10ccec5684d7335
delolmao <sonic190327@gmail.com>
2025-09-27 12:55:56 -0500

    Primera version del registro y menu

--- TP-POO1/TP_POO1.py
+++ TP-POO1/TP_POO1.py
@@ -1,1 +1,30 @@
-
+def RegistrarMascota(Especies):
+    NombresMascotas = []
+    Especie = str(input("Ingrese la especie de la mascota : "))
+    while True:
+        Mascota = str(input("Ingrese el nombre de la mascota : "))
+        NombresMascotas.append(Mascota)
+        Continuar = str(input("Desea agregar otra mascota de la misma especie (s/n) : "))
+        if Continuar != "s":
+            break
+    Especies[Especie] = NombresMascotas
+def main():
+    Especies = {}
+    while True:
+        print("""\n\tMENU VETERINARIA
+        -------------------------------------
+        1. Registrar Mascota
+        2. Registrar Propietario
+        3. Agendar Cita
+        4. Cancelar Cita
+        5. Ver Historial
+        6. Ver Registros
+        7. Salir
+        -------------------------------------
+        """)
+        Opcion = int(input("Elija opcion : "))
+        if Opcion == 1:
+            RegistrarMascota(Especies)
+            print(Especies)
+if __name__ == "__main__" :
+    main()
